3083bba89722609fc6d66438f13dfb72f45780d3
ezgiergun <ezgiergun.k@gmail.com>
2023-09-21 17:53:30 +0300

    new parts added

--- js/main.js
+++ js/main.js
@@ -13,4 +13,17 @@ $(document).ready(function () {
 
     $(this).addClass("active-btn");
   });
+
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 300) {
+      $("#backToTop").fadeIn();
+    } else {
+      $("#backToTop").fadeOut();
+    }
+  });
+
+  $("#backToTop").click(function () {
+    $("html, body").animate({ scrollTop: 0 }, 800);
+    return false;
+  });
 });
